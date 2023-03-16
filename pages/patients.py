94c975c34b5405ece23f08816239d76aa6da5682
LuisM18 <LuisM18@github.com>
2023-03-16 15:12:21 -0500

    Correciones commit anterior patients

--- pages/patients.py
+++ pages/patients.py
@@ -1,11 +1,8 @@
 import pandas as pd  # 
 import plotly.express as px  # 
 import streamlit as st 
-<<<<<<< HEAD
-=======
 import mysql.connector
 import datetime as dt
->>>>>>> 6d4f5399 (Agreando informacion en patients)
 
 st.set_page_config(
     page_title="Pacientes",
@@ -13,36 +10,6 @@ st.set_page_config(
     layout="wide",
 )
 
-############## Cargar Data ###################################
-<<<<<<< HEAD
-labitems = pd.read_csv("./EDA_UCI/dataset/D_LABITEMS.csv")#
-drgcodes = pd.read_csv('./EDA_UCI/dataset/DRGCODES.csv')#
-labevents = pd.read_csv('./EDA_UCI/dataset/LABEVENTS.csv')#
-micro = pd.read_csv('./EDA_UCI/dataset/MICROBIOLOGYEVENTS.csv')#
-prescriptions = pd.read_csv('./EDA_UCI/dataset/PRESCRIPTIONS.csv')#
-procedures = pd.read_csv('./EDA_UCI/dataset/PROCEDURES_ICD.csv')#
-admissions = pd.read_csv('./EDA_UCI/dataset/ADMISSIONS.csv')
-callout = pd.read_csv('./EDA_UCI/dataset/CALLOUT.csv')
-icustays = pd.read_csv('./EDA_UCI/dataset/ICUSTAYS.csv')
-patients = pd.read_csv('./EDA_UCI/dataset/PATIENTS.csv')
-services = pd.read_csv('./EDA_UCI/dataset/SERVICES.csv')
-caregivers = pd.read_csv('./EDA_UCI/dataset/CAREGIVERS.csv')
-chartevents = pd.read_csv('./EDA_UCI/dataset/CHARTEVENTS.csv')
-datetimeevents = pd.read_csv('./EDA_UCI/dataset/DATETIMEEVENTS.csv')
-inputeventscv = pd.read_csv('./EDA_UCI/dataset/INPUTEVENTS_CV.csv')
-inputeventsmv = pd.read_csv('./EDA_UCI/dataset/INPUTEVENTS_MV.csv')
-transfers = pd.read_csv('./EDA_UCI/dataset/TRANSFERS.csv')
-dcpt = pd.read_csv('./EDA_UCI/dataset/D_CPT.csv')
-ddiagnoses = pd.read_csv('./EDA_UCI/dataset/D_ICD_DIAGNOSES.csv')
-dprocedures = pd.read_csv('./EDA_UCI/dataset/D_ICD_PROCEDURES.csv')#
-ditems = pd.read_csv('./EDA_UCI/dataset/D_ITEMS.csv')
-dlabitems = pd.read_csv('./EDA_UCI/dataset/D_LABITEMS.csv')
-noteevents= pd.read_csv('./EDA_UCI/dataset/NOTEEVENTS.csv')
-outputevents= pd.read_csv('./EDA_UCI/dataset/OUTPUTEVENTS.csv')
-procedureevents_mv = pd.read_csv('./EDA_UCI/dataset/PROCEDUREEVENTS_MV.csv')
-cptevents = pd.read_csv('./EDA_UCI/dataset/CPTEVENTS.csv')
-diagnoses_icd = pd.read_csv('./EDA_UCI/dataset/DIAGNOSES_ICD.csv') 
-=======
 mydb = mysql.connector.connect(
   host="proyectdb.mysql.database.azure.com",
   user="administrador123",
@@ -53,7 +20,6 @@ mydb = mysql.connector.connect(
 admissions = pd.read_sql("""SELECT subject_id
                             FROM admissions""",mydb)
 
->>>>>>> 6d4f5399 (Agreando informacion en patients)
 ###################################################################################
 
 st.markdown("# **Reporte de Paciente**")
@@ -89,11 +55,6 @@ st.dataframe(icustays)
 ###################################################################################
 st.markdown('## Historial clinico en la UCI')
 st.markdown("---")
-
-<<<<<<< HEAD
-st.subheader('Costos del paciente')
-st.markdown("---")
-=======
 
 diagnoses_icd = pd.read_sql("""SELECT d.seq_num, d.subject_id, d.hadm_id,dd.long_title 
                               FROM diagnoses_icd d
@@ -227,4 +188,4 @@ if st.button('Descargar'):#Centrar boton en el layout
   st.write(dt.datetime.now())
    
  
->>>>>>> 6d4f5399 (Agreando informacion en patients)
+
