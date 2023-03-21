e9ce08f15ac44adc05c8ebe20c8da64518ab5ca5
LuisM18 <LuisM18@github.com>
2023-03-21 11:25:01 -0500

    Corrigiendo errores

--- pages/_💹_analisis.py
+++ pages/_💹_analisis.py
@@ -15,7 +15,7 @@ mydb = mysql.connector.connect(
 
 ################## Cargar Data ##########################
 admissions = pd.read_sql("""SELECT *
-                            FROM admissions""",mydb,parse_dates=['INTIME','OUTTIME'])
+                            FROM admissions_hechos""",mydb,parse_dates=['INTIME','OUTTIME'])
 
 icustays = pd.read_sql("""SELECT *
                             FROM icustays""",mydb)
@@ -51,7 +51,7 @@ st.header('Análisis descriptivo')
 st.markdown("---")
 if tabla_seleccionada == 'patients':
    
-    adm_filter = st.selectbox("Selecciona el tipo de admisión", pd.unique(admissions["admission_type"]),key="1")
+    adm_filter = st.selectbox("Selecciona el tipo de admisión", pd.unique(admissions["ADMTYPE_ID"]),key="1")
     insurance = st.selectbox("Selecciona el seguro del paciente", pd.unique(admissions['INSURANCE']),key="2")
     # selecciona la etnia
     etnia = st.multiselect(
@@ -61,7 +61,7 @@ if tabla_seleccionada == 'patients':
     placeholder = st.empty()
 
     # dataframe filter
-    admissions = admissions[(admissions["admission_type"] == adm_filter) & (admissions["INSURANCE"] == insurance) & (admissions["ethnicity"].isin(etnia))]
+    admissions = admissions[(admissions["ADMTYPE_ID"] == adm_filter) & (admissions["INSURANCE"] == insurance) & (admissions["ethnicity"].isin(etnia))]
     fig3, fig4 = st.columns(2)
     fig5, fig6 = st.columns(2)
 
